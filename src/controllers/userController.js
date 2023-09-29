f40b4a6ad6f03ad2841449bd66c2274bb45d2076
PasinduK98 <pkbiz1998@gmail.com>
2023-09-30 03:58:17 +0530

    feat: Add endpoint to update user details by ID

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,7 +12,7 @@ const createUser = async (req, res) => {
   }
 };
 
-// Controller function to create a new user
+// Controller function to get user details by ID
 const getUserById = async (req, res) => {
   try {
     const { id } = req.params;
@@ -23,7 +23,24 @@ const getUserById = async (req, res) => {
   }
 };
 
+// Controller function to update the user details 
+
+const updateUser = async(req,res) =>{
+  try {
+    const {id} = req.params;
+    const user = await User.findByIdAndUpdate(id,req.body);
+    if(!user){
+      return res.status(404).json({message:`Cannot find the user with ID ${id}`});
+    }
+    const updatedUser = await User.findById(id);
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
 module.exports = {
   createUser,
-  getUserById
+  getUserById,
+  updateUser,
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,17 @@
 const express = require("express");
 
-const { createUser, getUserById } = require("../controllers/userController");
+const {
+  createUser,
+  getUserById,
+  updateUser,
+} = require("../controllers/userController");
 
 const router = express.Router();
 
 // Routes to handle the CRUD Operations
 router.post("/", createUser);
 router.get("/:id", getUserById);
+router.put("/:id",updateUser)
 
 
 
